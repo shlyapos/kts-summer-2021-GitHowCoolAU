864fdab1a9e2d16e6194811eeecb6ceafb66a628
Alexandr <alexandrsuchkov67@gmail.com>
2021-08-21 12:22:28 +0300

    fix little mistake

--- git-how-cool-au/src/App.tsx
+++ git-how-cool-au/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import { gitHubApp } from './root/root';
 
 function App() {
-  gitHubApp.getRepositoriesByUsername('jopashlyapos')
+  gitHubApp.getRepositoriesByUsername('shlyapos')
     .then(response => console.log(response));
 
   return (
