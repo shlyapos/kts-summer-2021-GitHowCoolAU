dc465b2d6c57ff38602385a1906e11a772bcc635
Alexandr <alexandrsuchkov67@gmail.com>
2021-09-03 20:38:06 +0300

    moved variables in special file

--- git-how-cool-au/src/index.tsx
+++ git-how-cool-au/src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import App from "@app/App";
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import './styles/variables.css';
 
 import reportWebVitals from './reportWebVitals';
 
