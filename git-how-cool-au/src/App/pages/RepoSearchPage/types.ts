99a18ed9217524fabfffe72f4ae8567096952b0b
Alexandr <alexandrsuchkov67@gmail.com>
2021-09-22 11:58:36 +0300

    deleted some excess types

--- git-how-cool-au/src/App/pages/RepoSearchPage/types.ts
+++ git-how-cool-au/src/App/pages/RepoSearchPage/types.ts
@@ -1,10 +1,5 @@
 import { RepoItemProps } from "@components/RepoItem";
 
-export type RepoListItem = {
-    id: string,
-    props: RepoItemProps
-};
-
 export type ReposInfoResponseSuccess = {
     id: string,
     name: string,
@@ -19,11 +14,4 @@ export type ReposInfoResponseSuccess = {
 
 export type ReposInfoResponseError = {
     message: string
-};
-
-export type RepoListContext = {
-    list: null | RepoListItem[],
-    isLoading: boolean,
-    load: (state: boolean) => void,
-    isAllLoad: boolean
 };
