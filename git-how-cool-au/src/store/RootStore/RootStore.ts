21c89c98c66b50152c082b6f0681e0b5140f98b7
Alexandr <alexandrsuchkov67@gmail.com>
2021-09-26 15:11:37 +0300

    added rootStore

---
+++ git-how-cool-au/src/store/RootStore/RootStore.ts
@@ -0,0 +1,14 @@
+import ApiStore, { ApiResponse, RequestParams } from "@store/RootStore/ApiStore";
+
+import QueryParamsStore from "./QueryParamsStore";
+
+const BASE_URL_GITHUB = 'https://api.github.com/';
+
+export default class RootStore {
+    readonly query = new QueryParamsStore();
+    private readonly apiStore = new ApiStore(BASE_URL_GITHUB);
+
+    async apiRequest<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>> {
+        return await this.apiStore.request(params);
+    }
+};
