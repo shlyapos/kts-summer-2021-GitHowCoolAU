bba065fcb68fcccb08a5a202a63a0a63cf8f0959
Alexandr <alexandrsuchkov67@gmail.com>
2021-09-26 15:15:04 +0300

    tried use query

--- git-how-cool-au/src/App/App.tsx
+++ git-how-cool-au/src/App/App.tsx
@@ -1,19 +1,21 @@
 import './App.css';
 
 import RepoSearchRoute from '@config/routes/RepoSearchRoute';
-import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import { useQueryParamsStoreInit } from '@store/RootStore/hooks/UseQueryParamsStoreInit';
+import { Switch, Redirect } from 'react-router-dom';
 
 import RepoSearchPage from './pages/RepoSearchPage';
 
+
 const App = () => {
+  useQueryParamsStoreInit();
+  
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <RepoSearchRoute path={'/repos/:owner?/:name?'}><RepoSearchPage /></RepoSearchRoute>
-          <Redirect to='/repos' />
-        </Switch>
-      </ BrowserRouter>
+      <Switch>
+        <RepoSearchRoute path={'/repos/:owner?/:name?'}><RepoSearchPage /></RepoSearchRoute>
+        <Redirect to='/repos' />
+      </Switch>
     </div>
   );
 }
